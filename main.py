36d0b2529b8b2cc75b49fcc0b4564bf7d18da1d3
Daniil <sychkov.daniil@yandex.ru>
2022-08-15 15:00:09 +0300

    main.py optimization

--- main.py
+++ main.py
@@ -40,8 +40,7 @@ def ship_field(ship):
 class Battlefield:
     def __init__(self, team_1, team_2):
         # Инициализация команд
-        self.team_1 = team_1
-        self.team_2 = team_2
+        self.team_1, self.team_2 = team_1, team_2
 
     def mainloop(self):
         while True:
