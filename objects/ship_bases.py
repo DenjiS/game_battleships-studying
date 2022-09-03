961f01c502f021e0dc57bc4ca9ce819cedd2ef9d
Daniil <sychkov.daniil@yandex.ru>
2022-09-03 22:19:58 +0300

    better target choosing

--- objects/ship_bases.py
+++ objects/ship_bases.py
@@ -61,7 +61,7 @@ class SupportShip(Ship):
             self.shield.team_buff(self)
 
     def heal_ally(self):
-        targets = [i for i in self.team.ships if i is not self and i.health < i.MAX_HEALTH]
+        targets = [i for i in self.team.ships if i is not (self and None) and i.health < i.MAX_HEALTH]
         if targets:
             ally = choice(targets)
             hp_to_heal = self.repair_team.diagnostics(ally)
