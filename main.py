bbe10b787aaa19efa3b85093e5a602babd201d60
Daniil <sychkov.daniil@yandex.ru>
2022-09-03 17:54:23 +0300

    Structure reformat for better comparison with versions 1.3 in GitHub gui

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from time import sleep
 from colorama import Fore, Style
 import os
 
-# Типы кораблей
 ship_types_list = [Jet, HeavyJet, Cruiser, CargoShip, RepairShip]
 
 
@@ -16,17 +15,45 @@ class Team:
         self.size = self.SIZE
 
         self.ships = []
+        # Ships initialization
         for num in range(self.SIZE):
-            # Создание объекта корабля
             append_ship = choice(ship_types_list)(self, num)
             self.ships.append(append_ship)
 
 
 class Battlefield:
     def __init__(self, *teams):
-        # Инициализация команд
         self.teams = teams
         self.running = True
+
+    # Methods used in Battlefield.screen()
+    @classmethod
+    def clear_screen(cls):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    @classmethod
+    def space(cls, obj):
+        return ' ' * (65 - len(obj))
+
+    @classmethod
+    def ship_field(cls, ship):
+        if ship:
+            space_ship_field = 25 - len(ship.name)
+            ship_hp = f'{ship.health}\\{ship.MAX_HEALTH}\\{ship.armor}'
+            return ship.name + '_' * space_ship_field + ship_hp
+        elif not ship:
+            return Fore.LIGHTBLACK_EX + '*' * 10 + '_' * 6 + '\\' * 8 + Style.RESET_ALL  # Death string
+
+    def screen(self):
+        self.clear_screen()
+        print('\n')
+        print(self.teams[0].name + self.space(self.teams[0].name) + self.teams[1].name)
+
+        for i in range(Team.SIZE):
+            ship_1, ship_2 = self.teams[0].ships[i], self.teams[1].ships[i]
+            string_1, string_2 = self.ship_field(ship_1), self.ship_field(ship_2)
+
+            print(string_1 + self.space(string_1) + string_2)
 
     def battle_gen(self):
         cursor = 0
@@ -44,6 +71,16 @@ class Battlefield:
             else:
                 cursor += 1
 
+    def endgame(self):
+        self.clear_screen()
+        winner = self.teams[0] if any(self.teams[0].ships) else self.teams[1]
+        print('\n')
+        print(f'{winner.name} is winner')
+        self.screen()
+        print('\n')
+        print('FINISH')
+        self.running = False
+
     def mainloop(self):
         gen = self.battle_gen()
         while self.running:
@@ -56,57 +93,6 @@ class Battlefield:
 
             except StopIteration:
                 self.endgame()
-
-    @classmethod
-    def clear_screen(cls):
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-    @classmethod
-    def space(cls, obj):
-        """Метод для формирования ровной колонки правой команды при отображении Battlefield.screen;
-        :param obj: str - текст левой команды
-        :return: str - необходимое кол-во пробелов перед текстом правой команды
-        """
-        return ' ' * (65 - len(obj))
-
-    @classmethod
-    def ship_field(cls, ship):
-        """Поле корабля для Battlefield.screen;
-        :param ship: obj - объект корабля
-        :return: str - название, здоровье, макс. здоровье
-        """
-        if ship:
-            space_ship_field = 25 - len(ship.name)
-            ship_hp = f'{ship.health}\\{ship.MAX_HEALTH}\\{ship.armor}'
-            return ship.name + '_' * space_ship_field + ship_hp
-        elif not ship:
-            return Fore.LIGHTBLACK_EX + '*' * 10 + '_' * 6 + '\\' * 8 + Style.RESET_ALL  # Death string
-
-    def screen(self):
-        """
-        Функция отрисовывает построчно поле игры, собирая в итерации цикла строку из аттрибутов объектов кораблей
-        :return: Визуальное отображение игры в консоли
-        """
-        self.clear_screen()
-        # Отрисовка
-        print('\n')
-        print(self.teams[0].name + self.space(self.teams[0].name) + self.teams[1].name)
-
-        for i in range(Team.SIZE):
-            ship_1, ship_2 = self.teams[0].ships[i], self.teams[1].ships[i]
-            string_1, string_2 = self.ship_field(ship_1), self.ship_field(ship_2)
-
-            print(string_1 + self.space(string_1) + string_2)
-
-    def endgame(self):
-        self.clear_screen()
-        winner = self.teams[0] if any(self.teams[0].ships) else self.teams[1]
-        print('\n')
-        print(f'{winner.name} is winner')
-        self.screen()
-        print('\n')
-        print('FINISH')
-        self.running = False
 
 
 if __name__ == '__main__':
